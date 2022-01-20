ee25b16cf9a465dfb1b430ff1d2d0f5f9a751a14
barman47 <nomsouzoanya@yahoo.co.uk>
2022-01-20 17:22:00 +0100

    Minor Updates

--- src/actions/myLocation.js
+++ src/actions/myLocation.js
@@ -4,7 +4,7 @@ import { SET_MY_LOCATION } from './types';
 
 export const getMyLocation = () => async dispatch => {
     try {
-        const res = await axios.get('http://ip-api.com/json');
+        const res = await axios.get('https://ip-api.com/json');
         dispatch({
             type: SET_MY_LOCATION,
             payload: res.data
