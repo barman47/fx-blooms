f54e411e25893dcf70f448ed7a78e5f9983d18b6
barman47 <nomsouzoanya@yahoo.co.uk>
2022-07-21 15:08:48 +0100

    Minor Updates

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -6,6 +6,7 @@ const setAuthToken =  async (token) => {
     if (token) {
         // Apply to every request
         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        console.log('Setting token');
         axios.defaults.headers.common['Accept'] = 'application/json';
         // axios.defaults.headers.common['token'] = token;
         sessionStorage.setItem(AUTH_TOKEN, token);
